dda4936d01fd587bd3b17c3f7485fb3daf744b90
Shinyn <mattias.alm@hotmail.com>
2019-08-31 19:29:47 +0200

    Lion first commit

---
+++ Assets/Scripts/ButtonInput.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonInput : MonoBehaviour
+{
+    public enum Button
+    {
+        topLeft,
+        topRight,
+        bottomLeft,
+        bottomRight
+    };
+
+    public Button button;
+
+    public delegate void ButtonPressed();
+    public static event ButtonPressed OnLeftUpPressed;
+    
+
+    private void OnMouseDown()
+    {
+        if (OnLeftUpPressed != null && button == Button.topLeft)
+        {
+            OnLeftUpPressed();
+            //Debug.Log("top left");
+        }
+        else if (button == Button.bottomLeft)
+        {
+            //Debug.Log("bottom left");
+        }
+        else if (button == Button.topRight)
+        {
+            //Debug.Log("top right");
+        }
+        else if (button == Button.bottomRight)
+        {
+            //Debug.Log("bottom right");
+        }
+        
+    }
+}
---
+++ Assets/Scripts/LionTamerController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LionTamerController : MonoBehaviour
+{
+    public List<Transform> positions = new List<Transform>();
+    int currentPositionLeftSide = 1;
+    int currentPositionRightSide = 0;
+
+    void Start()
+    {
+        UpdateLeftPosition();
+        UpdateRightPosition();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        ButtonInput.OnLeftUpPressed += OnLeftUpPressed;
+    }
+
+    private void OnDisable()
+    {
+        ButtonInput.OnLeftUpPressed -= OnLeftUpPressed;
+    }
+    private void UpdateLeftPosition()
+    {
+        transform.position = positions[currentPositionLeftSide].position;
+        Debug.Log("update pos left");
+    }
+
+    private void UpdateRightPosition()
+    {
+        transform.position = positions[currentPositionRightSide].position;
+        Debug.Log("update pos right");
+    }
+
+    private void OnLeftUpPressed()
+    {
+        if (currentPositionLeftSide > 0)
+        {
+            currentPositionLeftSide--;
+            UpdateLeftPosition();
+        }
+    }
+
+    private void OnLeftDownPressed()
+    {
+        if (currentPositionLeftSide < positions.Count - 1)
+        {
+            currentPositionLeftSide++;
+            UpdateLeftPosition();
+        }
+    }
+
+    private void OnRightUpPressed()
+    {
+        if (currentPositionRightSide > 0)
+        {
+            currentPositionRightSide--;
+            UpdateRightPosition();
+        }
+    }
+
+    private void OnRightDownPressed()
+    {
+        if (currentPositionRightSide < positions.Count - 1)
+        {
+            currentPositionRightSide++;
+            UpdateRightPosition();
+            Debug.Log("rightDown updated");
+        }
+    }
+}
